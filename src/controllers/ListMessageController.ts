960948bcc18a54a5e6aff07c3899b772dde3e49a
Vinícius Fernandes <viniciusfernandesmct@gmail.com>
2022-01-18 23:55:31 -0300

    updated: changed 'hanle' to 'handle'

--- src/controllers/ListMessageController.ts
+++ src/controllers/ListMessageController.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import { ListMessageService } from "../services/ListMessagesService";
 
 class ListMessageController {
-    async hanle(request: Request, response: Response){
+    async handle(request: Request, response: Response){
         const listMessageService = new ListMessageService();
 
         const allMessages = await listMessageService.execute();
